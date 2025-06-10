7ac56d80f8ce731e35f0542071bf015077dbdc64
lilianantw <liliana.ntw@gmail.com>
2025-06-10 18:25:06 +0000

    Deploying to gh-pages from @ lilianantw/TwelveTech@5ef1ca3a0bfbccd539c9a461b27f961e1725cf23 🚀

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ import{a as m,i as w,S as k}from"./assets/vendor-rzQNFhEO.js";(function(){const 
     </div>
   `,t}function N(e){let n="";for(let t=1;t<=5;t++){const a=t<=e?"star-filled":"star-outline";n+=`
       <svg class="star-icon ${a}" width="24" height="24">
-        <use href="img/symbol-defs.svg#${t<=e?"icon-star-filled":"icon-star-outline"}"></use>
+        <use href="/img/symbol-defs.svg#${t<=e?"icon-star-filled":"icon-star-outline"}"></use>
       </svg>
     `}return n}function O(){const e=new k(".feedback-swiper",{loop:!1,navigation:{nextEl:".feedback-button-next",prevEl:".feedback-button-prev"},pagination:{el:".feedback-pagination",clickable:!0,type:"custom",renderCustom:function(s,n,t){const o=t-1;let r="",l="",i="";return n-1===0?r="swiper-pagination-bullet-active":n-1===o?i="swiper-pagination-bullet-active":l="swiper-pagination-bullet-active",`
           <span class="swiper-pagination-bullet ${r}" data-slide-index="0"></span>
