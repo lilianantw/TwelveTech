0a37665aafa600d25d1d8e356720d0f36d73417b
Volodymyr Mazun <mebelmv1990@gmail.com>
2025-06-11 23:23:56 +0300

    correct background

--- src/js/hero.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.main-button')
-    
-button.addEventListener('click', function (e) {
-    e.preventDefault();
-document.querySelector('#artist-section').scrollIntoView({ behavior: 'smooth' });
-});
